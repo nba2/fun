9e66de6488904e94d6661c32a06d48477a984579
Noah Andrew <nba2@williams.edu>
2018-09-16 13:35:25 -0400

    Adding tools.py.

--- tools.py
+++ tools.py
@@ -2,8 +2,10 @@ def changerto1(anymatrix, rowsvariable, columnsvariable):
 	original = anymatrix[0][0]
 	while anymatrix[0][0] != 1:
 		if anymatrix[0][0] == 0:
-			anymatrix[0][0] = anymatrix[0][0] + 1
-			anymatrix[0][columnsvariable-1] = anymatrix[0][columnsvariable-1] + 1
+			firstrow = anymatrix[0]
+			for i in range(rowsvariable-1):
+				anymatrix[i] = anymatrix[i + 1]
+				anymatrix[rowsvariable-1] = firstrow
 		else:
 			for i in range(columnsvariable):
 				anymatrix[0][i] = anymatrix[0][i]*(1/original)
