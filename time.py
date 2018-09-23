54c5a234f392cf34f66beaa13869fb4e474f613b
Noah Andrew <nba2@williams.edu>
2018-09-23 14:13:14 -0400

    Wrote a script that returns military time and days since 1/1/1970.

---
+++ time.py
@@ -0,0 +1,13 @@
+from time import time
+seconds = time()
+days = int(seconds//(24*60*60))
+seconds = seconds%(24*60*60)
+hours = int(seconds//(60*60))
+seconds = seconds%(60*60)
+minutes = int(seconds//60)
+if minutes < 10:
+    minutes = str(minutes)
+    minutes = "0" + minutes
+seconds = seconds%60
+seconds = int(seconds)
+print("The time is {}:{}:{}, {} days since the epoch.".format(hours,minutes,seconds,days))
