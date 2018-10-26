7c641a39e9f6817e53cb2d0ec656b8eab7bd646a
Noah Andrew <nba2@williams.edu>
2018-10-26 14:10:17 -0400

    Replaced kmeans with Duane's file (10/26/18).

--- Class Examples/kmeans.py
+++ Class Examples/kmeans.py
@@ -1,37 +1,45 @@
-# A clustering data structure implementing k-means
-# (c) 2018 noah andrew
+# A clustering data structure implementing k-means.
+# (c) 2018
 
 from random import randint
 
-__all__ [ "KMeans" ]
+__all__ = [ "KMeans", "fdist", "fmean" ]
 
 def fdist(a,b):
+    """A distance function for float values."""
     return abs(a-b)
 
-def meanf(s):
+def fmean(s):
+    """A Mean function for float values."""
+    assert(s)
     return sum(s)/len(s)
 
 class KMeans(object):
-    """An implementation of k-means clustering. Typical use:
-    data = [ randint(0,1000000) for _ in range (1000) ]
+    """An implementation of k-means clustering.  Typical use:
+    data = [ randint(0,1000000) for _ in range(1000) ]
     cl = KMeans(data, k = 10)
     for rep in cl.means():
-        print ("Cluster {}:".format(rep))
+        print("Cluster {}:".format(rep))
         for value in cl.cluster(rep):
             print("  {}".format(value))
     """
-
     __slots__ = [ "_means", "_k", "_clusters", "_distf", "_meanf", "_data" ]
 
     def __init__(self, data, k = 5, distf = None, meanf = None):
+        """Load a cluster data into k clusters.  The distf and meanf
+           functions, if not specified, default to distance and mean
+           computations on floating point values."""
         self._distf = fdist if distf is None else distf
         self._meanf = fmean if meanf is None else meanf
         self._k = k
         self._data = list(data)
+        # values we compute
+        self._clusters = []
         self._cluster()
 
     def _pickMeans(self):
-        """Select k "representative values" from the data."""
+        """Select some initial means.  Here, we pick k random values from
+        the original dataset.  We try to avoid repeats."""
         n = len(self._data)
         locations = []
         means = []
@@ -41,26 +49,54 @@ class KMeans(object):
                 loc = randint(0,n-1)
             locations.append(loc)
             means.append(self._data[loc])
-        return means
+        self._means = means
 
-    def _findClosest(self, value):
-        min = 0
-        for ind in range (self._k):
-            rep = self._means[ind]
-            if self._distf(rep, value) < self._means[min]:
-                min = ind
-        return self._means[min]
+    def _findClosest(self,value):
+        """Find the cluster number that has a representative that is closest
+           to value."""
+        mindex = 0
+        minDist = self._distf(value,self._means[0])
+        for ind in range(1,self._k):
+            d = self._distf(value,self._means[ind])
+            if d < minDist:
+                mindex = ind
+                minDist = d
+        return mindex
 
     def _classify(self):
+        """For each data value, place it in a cluster which best represents
+           it; all data in a cluster are closest to their representative."""
+        self._clusters = [ [] for _ in range(self._k) ]
         for value in self._data:
-            rep = self._findClosest(value)
-            self._cluster[rep].append(value)
+            repIndex = self._findClosest(value)
+            self._clusters[repIndex].append(value)
+
+    def _adjustMeans(self):
+        """Compute means based on the current clustering."""
+        self._means = []
+        for i in range(self._k):
+            self._means.append(self._meanf(self._clusters[i]))
 
     def _cluster(self):
-        self._means = self.pickMeans()
+        """Iteratively find representative values, classify data and iterate."""
+        self._pickMeans()
+        self._classify()
         for _ in range(10):
-            self._classify()
             self._adjustMeans()
+            self._classify()
+
+    def means(self):
+        return self._means.copy()
+
+    def classify(self, value):
+        repIndex = self._findClosest(value)
+        return self._means[repIndex]
+
+    def cluster(self,rep):
+        if rep not in self._means:
+            rep = self.classify(rep)
+        return self._clusters[self._means.index(rep)]
+
 
 if __name__ == "__main__":
     data = [ randint(0,1000000) for _ in range(1000) ]
@@ -69,4 +105,4 @@ if __name__ == "__main__":
         print("Cluster {}:".format(rep))
         for value in cl.cluster(rep):
             print("  {}".format(value))
-    # rep = cl.classify(value)
+     # rep = cl.classify(value)
